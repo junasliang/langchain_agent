27047d37831f91abd4f2e695fd29ac8f5379eedd
junasliang <jun942670@gmail.com>
2026-04-14 17:40:37 +0800

    intro to search agents

---
+++ main.py
@@ -0,0 +1,48 @@
+from dotenv import load_dotenv
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+
+# from tavily import TavilyClient
+from langchain_tavily import TavilySearch
+
+load_dotenv()
+
+# tavily = TavilyClient()
+
+
+# @tool
+# def search(query: str) -> str:
+#     """
+#     Tool that searches over internet.
+#     Args:
+#         query: The query to search for.
+#     Return:
+#         The search result.
+#     """
+#     print(f"Searching for {query}")
+#     return tavily.search(query=query)
+
+
+llm = ChatOpenAI(model="gpt-5")
+tools = [TavilySearch()]
+agent = create_agent(model=llm, tools=tools)
+
+
+def main():
+    print("Hello from langchain-agent!")
+    result = agent.invoke(
+        {
+            "messages": [
+                HumanMessage(
+                    content="Search for 3 job posting for an ai engineer using langchain in the bay area on linked and list their details."
+                )
+            ]
+        }
+    )
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
