13b71510dc5e6986efb23c3f83d570de0859e556
junasliang <jun942670@gmail.com>
2026-04-20 11:37:36 +0800

    add tavily and search tool

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from langchain-agent!")
+# ruff: noqa: E402
+from dotenv import load_dotenv
 
+load_dotenv()
+
+from langchain_core.messages import HumanMessage
+from langchain_core.output_parsers.openai_tools import (
+    JsonOutputToolsParser,
+    PydanticToolsParser,
+)
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_openai import ChatOpenAI
 
 if __name__ == "__main__":
-    main()
+    print("Hello Reflexion Agent!")
